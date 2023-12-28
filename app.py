d644bf78e45e1c6d9dcda25068f21611ffa678c6
T Sumanth <69945453+Sumanth0601@users.noreply.github.com>
2023-12-28 18:51:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,114 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
+from pytube import YouTube
+from pydub import AudioSegment
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'mp3'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         youtube_link = request.form.get('link')
-        audio_file = request.files.get('audio-file')  # Use request.files to get the uploaded file
         summary_type = request.form.get('summary-type', 'normal')
         audio_language = request.form.get('audio-language', 'english')
 
-        if youtube_link and not audio_file:  # YouTube link is present and audio file is not present
-            print(f'YouTube Link: {youtube_link}')
-            print(f'Summary Type: {summary_type}')
-            print(f'Audio Language: {audio_language}')
+        if youtube_link:
+            try:
+                yt = YouTube(youtube_link)
+                audio_stream = yt.streams.filter(only_audio=True).first()
 
-            # You can process the YouTube link further or redirect to another page
-            return render_template('index.html', link=youtube_link, summary_type=summary_type, audio_language=audio_language, file_name=None)
+                if audio_stream:
+                    audio_stream.download(app.config['UPLOAD_FOLDER'])
+                    mp4_filename = audio_stream.default_filename
+                    mp4_filepath = os.path.join(app.config['UPLOAD_FOLDER'], mp4_filename)
+                    mp3_filename = mp4_filename.replace('mp4', 'mp3')
+                    mp3_filepath = os.path.join(app.config['UPLOAD_FOLDER'], mp3_filename)
 
-        elif audio_file and not youtube_link:  # Audio file is present and YouTube link is not present
-            file_name = audio_file.filename
-            print(f'Uploaded File: {file_name}')
-            print(f'Summary Type: {summary_type}')
-            print(f'Audio Language: {audio_language}')
+                    # Convert to MP3 using pydub
+                    audio = AudioSegment.from_file(mp4_filepath, format='mp4')
+                    audio.export(mp3_filepath, format='mp3')
 
-            # You can process the audio file further or redirect to another page
-            return render_template('index.html', link=None, summary_type=summary_type, audio_language=audio_language, file_name=file_name)
+                    # Remove the original MP4 file
+                    os.remove(mp4_filepath)
 
-        else:  # Handle the case when both YouTube link and audio file are present
-            print('Both YouTube Link and Audio File are present. Prioritizing YouTube Link.')
-            print(f'YouTube Link: {youtube_link}')
-            print(f'Summary Type: {summary_type}')
-            print(f'Audio Language: {audio_language}')
-
-            # You can process the YouTube link further or redirect to another page
-            return render_template('index.html', link=youtube_link, summary_type=summary_type, audio_language=audio_language, file_name=None)
+                    return render_template('index.html', link=youtube_link, summary_type=summary_type,
+                                           audio_language=audio_language, file_name=mp3_filename)
+                else:
+                    return render_template('index.html', link=None, error_message="No audio stream found.")
+            except Exception as e:
+                print(f"Error: {e}")
+                return render_template('index.html', link=None, error_message="Error processing YouTube link.")
+        else:
+            return render_template('index.html', link=None, error_message="Please enter a valid YouTube link.")
 
     else:
         return render_template('index.html', link=None)
 
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), as_attachment=True)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         youtube_link = request.form.get('link')
+#         summary_type = request.form.get('summary-type', 'normal')
+#         audio_language = request.form.get('audio-language', 'english')
+
+#         if youtube_link:
+#             try:
+#                 yt = YouTube(youtube_link)
+#                 audio_stream = yt.streams.filter(only_audio=True).first()
+
+#                 if audio_stream:
+#                     audio_stream.download(app.config['UPLOAD_FOLDER'])
+#                     mp4_filename = audio_stream.default_filename
+#                     mp4_filepath = os.path.join(app.config['UPLOAD_FOLDER'], mp4_filename)
+#                     mp3_filename = mp4_filename.replace('mp4', 'mp3')
+#                     mp3_filepath = os.path.join(app.config['UPLOAD_FOLDER'], mp3_filename)
+
+#                     # Convert to MP3 using pydub
+#                     audio = AudioSegment.from_file(mp4_filepath, format='mp4')
+#                     audio.export(mp3_filepath, format='mp3')
+
+#                     # Remove the original MP4 file
+#                     os.remove(mp4_filepath)
+
+#                     return render_template('index.html', link=youtube_link, summary_type=summary_type,
+#                                            audio_language=audio_language, file_name=mp3_filename)
+#                 else:
+#                     return render_template('index.html', link=None)
+#             except Exception as e:
+#                 print(f"Error: {e}")
+#                 return render_template('index.html', link=None)
+
+#         # No valid input
+#         else:
+#             return render_template('index.html', link=None)
+
+#     else:
+#         return render_template('index.html', link=None)
